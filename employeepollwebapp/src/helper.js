6e90198f6ecf5e06c7d03936f812a77c6d1fa03b
shehreen2022skipq <101668100+shehreen2022skipq@users.noreply.github.com>
2022-07-18 02:34:34 +0500

    Delete employeepollwebapp directory

--- employeepollwebapp/src/helper.js
+++
@@ -1,5 +0,0 @@
-export function formatDate(timestamp) {
-  const d = new Date(timestamp);
-  const time = d.toLocaleTimeString("en-US");
-  return time.substr(0, 5) + time.slice(-2) + " | " + d.toLocaleDateString();
-}
