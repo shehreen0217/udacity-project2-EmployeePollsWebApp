b0577edf6057180ae73ace53565d2ee4ddf2be9a
shehreen2022skipq <shehreen.fatima.skipq@gmail.com>
2022-07-18 20:52:56 +0500

    dashboard updated

--- .github/employeepollwebapp/src/components/Dashboard.js
+++ .github/employeepollwebapp/src/components/Dashboard.js
@@ -2,9 +2,20 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Poll from "./Poll";
 import { question } from "../Store/Actions/question";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
 const Dashboard = () => {
+  const [value, setValue] = useState("New");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
   const dispatch = useDispatch();
   const initialQuestions = useSelector((state) => state.questions.Questions);
   const currentUser = useSelector((state) => state.user.user);
@@ -26,23 +37,51 @@ const Dashboard = () => {
   unanswered.sort((x, y) => y.timestamp - x.timestamp);
 
   return (
-    <div data-testid="dashboard">
-      <h2 style={styles.heading}> New Questions</h2>
-      <div style={styles.div}>
-        {initialQuestions &&
-          unanswered.map((question) => {
-            return <Poll key={question.id} question={question} value="Poll" />;
-          })}
-      </div>
-      <br></br>
-      <h2 style={styles.heading}> Answered</h2>
-      <div style={styles.div}>
-        {initialQuestions &&
-          answered.map((question) => {
-            return <Poll key={question.id} question={question} value="View" />;
-          })}
-      </div>
-    </div>
+    <>
+      <FormControl>
+        <FormLabel id="demo-controlled-radio-buttons-group">Polls</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-controlled-radio-buttons-group"
+          name="controlled-radio-buttons-group"
+          value={value}
+          onChange={handleChange}
+        >
+          <FormControlLabel value="New" control={<Radio />} label="New Polls" />
+          <FormControlLabel
+            value="Answered"
+            control={<Radio />}
+            label="Answered Polls"
+          />
+        </RadioGroup>
+      </FormControl>
+
+      {value === "New" && (
+        <div>
+          <h2 style={styles.heading}> New Questions</h2>
+          <div style={styles.div}>
+            {initialQuestions &&
+              unanswered.map((question) => {
+                return (
+                  <Poll key={question.id} question={question} value="Poll" />
+                );
+              })}
+          </div>
+        </div>
+      )}
+      {value === "Answered" && (
+        <div>
+          <h2 style={styles.heading}> Answered</h2>
+          <div style={styles.div}>
+            {initialQuestions &&
+              answered.map((question) => {
+                return (
+                  <Poll key={question.id} question={question} value="View" />
+                );
+              })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
